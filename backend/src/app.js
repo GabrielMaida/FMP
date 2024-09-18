f27f5330a6c39b10bdab0715250dc4b2012f68c4
Gabriel Antonio <gabriel.antonio.dlm@gmail.com>
2024-09-17 23:13:31 -0300

    feat: Método GET Exercícios

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,19 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const exercicios = {}
+const exercicios = {'Flexão': 0,
+                    'Agachamento': 0,
+                    'Caminhada': 0,
+                    'Água': 0}
 
-app.get('/api/bemvindo', (req, res) => {
+app.get('/api/oi', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.get('/api/exercicios', (req, res) => {
+    res.send(exercicios);
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
-
