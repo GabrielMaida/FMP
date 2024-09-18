22986865ad8e414627fad7f0f968762b463a072a
Gabriel Antonio <gabriel.antonio.dlm@gmail.com>
2024-09-18 20:11:40 -0300

    Criação objeto usuário

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,26 +6,49 @@ const app = express();
 app.use(cors());
 
 const exercicios = [{
-                        'nome':'Flexão',
-                        'quantidade':0
-                    },
-                    {
-                        'nome':'Agachamento',
-                        'quantidade':0
-                    },
-                    {
-                        'nome':'Caminhada',
-                        'quantidade':0
-                    },
-                    {
-                        'nome':'Água',
-                        'quantidade':0
-                    }]
+        'idExercicio':1,
+        'nome':'Flexão',
+        'xpUnitario':10
+    },
+    {
+        'idExercicio':2,
+        'nome':'Agachamento',
+        'xpUnitario':10
+    },
+    {
+        'idExercicio':3,
+        'nome':'Caminhada',
+        'xpUnitario':150 //1km
+    },
+    {
+        'idExercicio':4,
+        'nome':'Água',
+        'xpUnitario':300 //1L
+    }]
+
+const usuario = {
+                idUsuario:1,
+                nome:'Usuário',
+                email:'usuario@gmail.com',
+                senha:'123456',
+                exercicios:exercicios,
+                xpTotal:0,
+                nivel:1,
+                xpProximoNivel:100,
+                ultimaAtualizacao:new Date()
+            }
 
 app.get('/api/exercicios', (req, res) => {
-    res.status(200).json(exercicios);
+    res.status(200).json(usuario);
 })
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
+
+
+/*
+releases
+estrutura de pastas
+
+*/
