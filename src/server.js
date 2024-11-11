3832937d0eab873529e8902b98f588b63c029255
Gabriel Antonio <gabriel.antonio.dlm@gmail.com>
2024-11-11 17:44:47 -0300

    update: atualizações

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://GabrielAntonio:cesusc2024@cluster0.vs1rl.mongodb.net/FMP';
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 
 // Conectar ao MongoDB
@@ -41,7 +41,7 @@ const User = mongoose.model('user', userSchema);
 // API para retornar todos os usuários
 app.get('/api/users', async (req, res) => {
     try {
-        const users = await User.find();
+        const users = await User.find().sort({ userId: 1 }); // Ordena em ordem crescente pelo campo userId
         
         // Para cada usuário, buscar os exercícios correspondentes
         const usersWithExercises = await Promise.all(users.map(async (user) => {
@@ -68,6 +68,7 @@ app.get('/api/user/:userId', async (req, res) => {
 
         if (user) {
             const exercises = await Exercise.find({ exerciseId: { $in: user.exercises } });
+
             res.status(200).json({
                 ...user.toObject(),
                 exercises: exercises.map(exercise => ({
@@ -87,6 +88,7 @@ app.get('/api/user/:userId', async (req, res) => {
 app.get('/api/exercises', async (req, res) => {
     try {
         const users = await Exercise.find();
+
         res.status(200).json(users);
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -108,35 +110,40 @@ app.get('/api/exercise/:exerciseId', async (req, res) => {
     };
 });
 
+// API para criar um novo usuário
+app.post('/api/user', async (req, res) => {
+    try {
+        newUser = {
+            userId: req.body.userId,
+            name: req.body.name,
+            email: req.body.email,
+            tel: req.body.tel,
+            exercises: [],
+            xpTotal: 0,
+            level: 1
+        }
+
+        const user = await User.create(newUser);
+
+        res.status(201).json({ message: `User ${req.body.userId} sucessfully created.`});
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// API para excluir um usuário específico pelo seu Id
+app.delete('/api/user/:userId', async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ userId: req.params.userId });
+
+        res.status(200).json({ message: `User ${req.params.userId} sucessfully deleted.`});
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 
 // Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-/*
-// Função para salvar os dados
-const saveUser = async () => {
-    // Salvar usuário
-    const user = {
-        userId: 1,
-        name: 'User 1',
-        email: 'user@gmail.com',
-        tel: '48912345678',
-        exercises: [2,3,6,8],
-        xpTotal: 0,
-        level: 1
-    };
-
-    await User.create(user);
-    console.log('Data saved sucessfully!');
-};
-
-
-// Executar a função de salvar dados
-saveUser().then(() => {
-    mongoose.connection.close();
-}).catch(err => {
-    console.error('Saving data error:', err);
-});*/
