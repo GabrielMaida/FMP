435c2612765ac61a426c7f4d2a0779d546a95bd8
Gabriel Antonio <gabriel.antonio.dlm@gmail.com>
2024-11-22 20:26:37 -0300

    CI: testes finais

--- tests/unit/server.test.js
+++ tests/unit/server.test.js
@@ -2,14 +2,13 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const app = require('../../src/server');
-require('dotenv').config();
 
 let mongoServer;
 
 beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
     const mongoUri = mongoServer.getUri();
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
 afterAll(async () => {
