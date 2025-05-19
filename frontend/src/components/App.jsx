bf8ecee49063287aa9a569368ddc9fc36a8c74ca
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2025-05-18 21:27:22 -0300

    update: Repos Structure Updates

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+	return (
+		<>
+			<div className="bg-blue-200 h-[100vh] pt-50">
+				<p className="font-bold text-2xl bg-red-300 hover:hidden rounded-lg mx-100">Click on the Vite and React logos to learn more</p>
+			</div>
+		</>
+	);
+}
